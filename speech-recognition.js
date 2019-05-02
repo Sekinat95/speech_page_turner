90b89d44d256293f15d097e7ba8282298680785b
sekinat <sekinat@localhost.localdomain>
2019-05-02 19:59:33 -0400

    changes

--- speech-recognition.js
+++ speech-recognition.js
@@ -2,40 +2,26 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
-let p = document.createElement('p');
-const words = document.querySelector('.words');
+const words = document.querySelector('.speech');
 const book = document.querySelector('.book');
-const page = document.querySelector('page')
-words.appendChild(p);
-
-recognition.addEventListener('result', e=>{
-    const transcript = Array.from(e.results)
-        .map(result=>result[0])
-        .map(result=>result.transcript)
-        .join('')
-    
-        console.log(transcript)
-    
-    
-        //not what I want to do but imma do this first
-    // p.textContent = transcript;
-    // if(e.results[0].isFinal){
-    //     p = document.createElement('p');
-    //     words.appendChild(p);
-    // }
+const page = document.querySelector('page');
+; 
+function listen(e){
    
-//what i really want to do
-    /**
-     * speech to action
-     * speech = recognition starts from the word"go to page"
-     * action = turn page(div of class 'page')
-     */
-   
-
-
-    });
+        const transcript = Array.from(e.results)
+        
+            .map(result=>result[0])
+            .map(result=>result.transcript)
+            .join('')
     
- console.log()    
+            if (transcript.includes('go to page')){
+                console.log(transcript)
+               
+            }
+            
+        }
+recognition.addEventListener('result', listen);
+   // console.log(transcript)   
 recognition.start();
 recognition.addEventListener('end', recognition.start);
 
